13c18eb77469b6009b4092245f605821244ee475
Patriarch Jeinalem Domingo <pjdomingo@exist.com>
2022-01-07 14:50:11 +0800

    Add final modifier to error variable

--- project-scheduler/src/main/java/Main.java
+++ project-scheduler/src/main/java/Main.java
@@ -3,6 +3,7 @@ import controller.ProjectPlanController;
 public class Main {
 
 
+
     public static void main(String[] args) {
         ProjectPlanController projectPlanController = new ProjectPlanController();
         projectPlanController.start();
